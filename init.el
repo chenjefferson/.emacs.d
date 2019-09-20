9071a150efbde9e1f83947433de7546e632c6abf
Jefferson William Chen <jeffwilliamchen@ieee.org>
2019-09-19 22:54:55 -0700

    backup and menubar settings

---
+++ init.el
@@ -0,0 +1,36 @@
+;; init.el
+;; Emacs the way I like it.
+;; by Jefferson Chen
+
+;;
+;; set up Org file init
+;;
+
+;; set up MELPA
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; bootstrap 'use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; load file
+(org-babel-load-file (expand-file-name "~/.emacs.d/orginit.org"))
+
+;; stuff below added by default
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (which-key use-package try org-bullets))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
