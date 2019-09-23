744a09fe7fbc5aecbe40974435ce656174cb4c50
Jefferson William Chen <jeffwilliamchen@ieee.org>
2019-09-23 10:29:30 -0700

    merged old init files

--- init.el
+++ init.el
@@ -27,10 +27,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key use-package try org-bullets))))
+ '(package-selected-packages
+   (quote
+    (beacon undo-tree one-themes auto-complete helm ace-window which-key use-package try org-bullets))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
